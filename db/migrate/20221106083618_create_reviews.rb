486e8b6b70fa5ca5dbdc16dace3b366bf91c760b
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-11-06 12:11:19 +0300

    updated file

--- db/migrate/20221106083618_create_reviews.rb
+++ db/migrate/20221106083618_create_reviews.rb
@@ -1,5 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-    
+    create_table :reviews do |t|
+    t.integer :product_id
+    t.string  :comment
+    t.integer :star_rating
+    t.integer :user_id
+    end
   end
 end
